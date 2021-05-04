c6ec3f7a2e5c4822b67e22309466ef85dd686afa
Ricky Chon <rickychon99@gmail.com>
2021-05-04 01:17:29 -0400

    Remove unneeded dependencies

--- src/entities/campus.js
+++ src/entities/campus.js
@@ -1,4 +1,4 @@
-export default buildCreateCampus = ({ name, imageURL, address, description }) =>
+export default buildCreateCampus = () =>
 {
     return createCampus = ({ name, imageURL, address, description }) =>
     {
