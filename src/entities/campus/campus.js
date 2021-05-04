f24914893939fc98cebb1f6900f7ae8321ac9dbc
Ricky Chon <rickychon99@gmail.com>
2021-05-04 01:41:39 -0400

    Add ID to campus entity

--- src/entities/campus/campus.js
+++ src/entities/campus/campus.js
@@ -1,12 +1,19 @@
-export default buildCreateCampus = () =>
+export default buildCreateCampus = ({ ID }) =>
 {
-    return createCampus = ({ name, imageURL, address, description }) =>
+    return createCampus = ({
+        name,
+        id = ID(),
+        imageURL,
+        address,
+        description
+    } = {}) =>
     {
         if(!name) throw new Error('Campus must have a name.');
         if(!address) throw new Error('Campus must have an address.');
 
         return Object.freeze({
             getName: () => name,
+            getID: () => id,
             getImageURL: () => imageURL || 'https://via.placeholder.com/150',
             getAddress: () => address,
             getDescription: () => description || ''
