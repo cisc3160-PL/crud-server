c47603f46a4744ae2fe0a48735110fae81016f2b
Ricky Chon <rickychon99@gmail.com>
2021-05-04 01:49:52 -0400

    Add controller for campus POST request

---
+++ src/controllers/campus/post-campus.js
@@ -0,0 +1,33 @@
+export default buildPostCampus = ({ addCampus }) =>
+{
+    return postCampus = async (httpRequest) =>
+    {
+        try
+        {
+            const { ...campusInfo } = httpRequest.body;
+            const newCampus = await addCampus({ ...campusInfo });
+
+            return {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                statusCode: 201,
+                body: { newCampus }
+            }
+        }
+        catch(err)
+        {
+            console.error(err);
+
+            return {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                statusCode: 400,
+                body: {
+                    error: err.message
+                }
+            }
+        }
+    }
+};
