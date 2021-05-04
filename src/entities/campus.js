fb1d77ade1e4bfd6ae3c0862cc3cc382d480c562
Ricky Chon <rickychon99@gmail.com>
2021-05-04 01:15:14 -0400

    Rename campus creation functions

--- src/entities/campus.js
+++ src/entities/campus.js
@@ -1,6 +1,6 @@
-export default createCampus = ({ name, imageURL, address, description }) =>
+export default buildCreateCampus = ({ name, imageURL, address, description }) =>
 {
-    return buildCampus = ({ name, imageURL, address, description }) =>
+    return createCampus = ({ name, imageURL, address, description }) =>
     {
         if(!name) throw new Error('Campus must have a name.');
         if(!address) throw new Error('Campus must have an address.');
